45268cd3b28022bbc8a243d6f41104d4d5a96890
Sanjay Chandrasekar <sanjay.chandrasekar@gmail.com>
2026-01-07 15:03:57 -0800

    part 1 working

---
+++ build/CMakeFiles/log-analyzer.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/sanjaychandrasekar/Code/Github/log-analyzer/apps/main.cpp" "CMakeFiles/log-analyzer.dir/apps/main.cpp.o" "gcc" "CMakeFiles/log-analyzer.dir/apps/main.cpp.o.d"
+  "/Users/sanjaychandrasekar/Code/Github/log-analyzer/src/parser/log_parser.cpp" "CMakeFiles/log-analyzer.dir/src/parser/log_parser.cpp.o" "gcc" "CMakeFiles/log-analyzer.dir/src/parser/log_parser.cpp.o.d"
+  "/Users/sanjaychandrasekar/Code/Github/log-analyzer/src/utils/memory_pool.cpp" "CMakeFiles/log-analyzer.dir/src/utils/memory_pool.cpp.o" "gcc" "CMakeFiles/log-analyzer.dir/src/utils/memory_pool.cpp.o.d"
+  "/Users/sanjaychandrasekar/Code/Github/log-analyzer/src/utils/streaming_reader.cpp" "CMakeFiles/log-analyzer.dir/src/utils/streaming_reader.cpp.o" "gcc" "CMakeFiles/log-analyzer.dir/src/utils/streaming_reader.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/log-analyzer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/log-analyzer.dir/apps/main.cpp.o"
+  "CMakeFiles/log-analyzer.dir/apps/main.cpp.o.d"
+  "CMakeFiles/log-analyzer.dir/src/parser/log_parser.cpp.o"
+  "CMakeFiles/log-analyzer.dir/src/parser/log_parser.cpp.o.d"
+  "CMakeFiles/log-analyzer.dir/src/utils/memory_pool.cpp.o"
+  "CMakeFiles/log-analyzer.dir/src/utils/memory_pool.cpp.o.d"
+  "CMakeFiles/log-analyzer.dir/src/utils/streaming_reader.cpp.o"
+  "CMakeFiles/log-analyzer.dir/src/utils/streaming_reader.cpp.o.d"
+  "log-analyzer"
+  "log-analyzer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/log-analyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
